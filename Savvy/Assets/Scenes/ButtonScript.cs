33b988441a60e9c94757dc0502726aad4ac95612
mjongithub1 <mjmurray5474@gmail.com>
2024-06-18 16:35:03 -0700

    Block spins now

--- Savvy/Assets/Scenes/ButtonScript.cs
+++ Savvy/Assets/Scenes/ButtonScript.cs
@@ -1,13 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
+using JetBrains.Annotations;
 using Unity.VisualScripting;
 using UnityEngine;
 
 public class ButtonScript : MonoBehaviour
 {
-    public void Testfun() {
-        print("I EXIST");
+
+    public bool isPressed;
+
+    public GameObject cube;
+    float degrees = 0;
+    public float degreeChange = 45;
+    
+    
+    public void Update() {
+
+        if(isPressed == true)
+        { 
+            degrees += degreeChange;
+            Quaternion rotation = Quaternion.Euler(degrees, 0, 0);
+
+            cube.transform.rotation = rotation;
+
+
+            print("I EXIST");
+        }
+
     }
 
-    
+
+    public void MOUSEUP()
+    {
+        isPressed = false;
+    }
+
+    public void MOUSEDOWN()
+    {
+        isPressed = true;
+    }
+
+
 }
