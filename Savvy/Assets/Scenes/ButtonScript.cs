ba31e8df9db74df616c5643a3318bbc672d0412a
mjongithub1 <mjmurray5474@gmail.com>
2024-06-17 17:24:49 -0700

    button clicks and semi worls

---
+++ Savvy/Assets/Scenes/ButtonScript.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class ButtonScript : MonoBehaviour
+{
+    public void Testfun() {
+        print("I EXIST");
+    }
+}
